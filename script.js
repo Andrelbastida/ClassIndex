7280b7df8b60b8f7cd03830b64b35cf0ee066635
andrelbastida <andrelbastida@yahoo.com.br>
2023-05-16 20:00:15 -0300

    v

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const galleryContainer = document.querySelector('.gallery-container');
-const galleryControlsContainer= document.querySelector('.gallery-controls');
-const galleryControls = ['Voltar','Próximo'];
-const galleryItems= document.querySelectorAll('.gallery-item');
+const galeriaContainer = document.querySelector('.galeria-container');
+const containerControlesGaleria= document.querySelector('.galeria-controle');
+const galeriaControle = ['Voltar','Próximo'];
+const galeriaItens= document.querySelectorAll('.galeria-item');
 
 class Carousel{
 
     constructor(container, items, controls){
         this.carouselContainer= container;
-        this.carouselControls = controls;
+        this.controleCarrosel = controls;
         this.carouselArray = [...items];
     }
 
     updateGallery(){
         this.carouselArray.forEach(el => {
-            el.classList.remove('gallery-item-1');
-            el.classList.remove('gallery-item-2');
-            el.classList.remove('gallery-item-3');
-            el.classList.remove('gallery-item-4');
-            el.classList.remove('gallery-item-5');
+            el.classList.remove('galeria-item-1');
+            el.classList.remove('galeria-item-2');
+            el.classList.remove('galeria-item-3');
+            el.classList.remove('galeria-item-4');
+            el.classList.remove('galeria-item-5');
 
         });
         this.carouselArray.slice(0, 5).forEach((el , i) => {
-            el.classList.add(`gallery-item-${i+1}`);
+            el.classList.add(`galeria-item-${i+1}`);
         });
     }
 
     setCurrentState(direction){
-        if(direction.className == 'gallery-controls-Voltar'){
+        if(direction.className == 'galeria-controle-Voltar'){
          this.carouselArray.unshift(this.carouselArray.pop());
           }else{
             this.carouselArray.push(this.carouselArray.shift());
@@ -34,14 +34,14 @@ class Carousel{
         this.updateGallery();
     }
     setControls(){
-        this.carouselControls.forEach(control => {
-            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
-            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        this.controleCarrosel.forEach(control => {
+            containerControlesGaleria.appendChild(document.createElement('button')).className = `galeria-controle-${control}`;
+            document.querySelector(`.galeria-controle-${control}`).innerText = control;
         });
     }
 
     useControls(){
-        const triggers = [...galleryControlsContainer.childNodes];
+        const triggers = [...containerControlesGaleria.childNodes];
         triggers.forEach(control =>{
             control.addEventListener('click', e => {
                 e.preventDefault();
@@ -52,12 +52,8 @@ class Carousel{
 
     }
 
-    mudarBackground(imagem) {
-        document.body.style.backgroundImage = "url('" + imagem + "')";
-      }
-
 }
-const exampleCarousel = new Carousel(galleryContainer, galleryItems,galleryControls);
+const exampleCarousel = new Carousel(galeriaContainer, galeriaItens,galeriaControle);
 
 exampleCarousel.setControls();
 exampleCarousel.useControls();
